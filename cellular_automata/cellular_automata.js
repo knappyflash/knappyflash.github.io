c25dc1811fc007e0c5961e97dc3faa6f218e44d8
knappyflash <knappyflash2@gmail.com>
2026-06-03 17:42:05 -0700

    cell automata

--- cellular_automata/cellular_automata.js
+++ cellular_automata/cellular_automata.js
@@ -8,21 +8,34 @@ let cols = canvSize[0] / cellSize;
 let rows = canvSize[1] / cellSize;
 
 function setup() {
-  let canvas = createCanvas(canvSize[0], canvSize[1]);
-  canvas.parent("cellularAutomataDiv");
-  background(bg[0], bg[1], bg[2], bg[3]);
+  ClearGrid();
+  grid[0][37] = 1;
+  RunAutomata();
+}
 
-  colorOn = color(255, 204, 0);
-  colorOff = color(88, 255, 237);
+function draw() {
+  // not needed for static version
+}
 
+function ClearGrid(){
   for (let row = 0; row < rows; row++) {
     grid[row] = [];
     for (let col = 0; col < cols; col++) {
       grid[row][col] = 0;
     }
   }
+}
 
-  grid[0][37] = 1;
+function RunAutomata(){
+  let canvas = createCanvas(canvSize[0], canvSize[1]);
+  canvas.parent("cellularAutomataDiv");
+  background(bg[0], bg[1], bg[2], bg[3]);
+
+  colorOn = color(255, 204, 0);
+  colorOff = color(88, 255, 237);
+  colorFirstRowOn = color(255, 255, 255);
+  colorFirstRowOff = color(0, 0, 0);
+  strokeWeight(1);
 
   for (let row = 0; row < rows - 1; row++) {
     for (let col = 0; col < cols; col++) {
@@ -30,14 +43,19 @@ function setup() {
       let center = grid[row][col];
       let right  = (col < cols - 1) ? grid[row][col + 1] : 0;
       grid[row + 1][col] = NextState(left, center, right);
-      fill(center === 1 ? colorOn : colorOff);
+
+      if (row == 0){
+        
+        stroke(100, 100, 100);
+        fill(center === 1 ? colorFirstRowOn : colorFirstRowOff);
+      } else{
+        stroke('black');
+        fill(center === 1 ? colorOn : colorOff);
+      }
+      
       rect(col * cellSize, row * cellSize, cellSize, cellSize);
     }
   }
-}
-
-function draw() {
-  // not needed for static version
 }
 
 function NextState(left, center, right) {
@@ -55,4 +73,19 @@ function NextState(left, center, right) {
     case "110": return 0;
     case "111": return 0;
   }
+}
+
+function mousePressed() {
+  if ((mouseY > 0) && (mouseY < cellSize)){
+    let col = Math.floor(mouseX / cellSize);
+    if (grid[0][col] == 0){
+      grid[0][col] = 1
+    } else{
+      grid[0][col] = 0
+    }
+    RunAutomata();
+  }
+}
+
+function mouseReleased() {
 }
