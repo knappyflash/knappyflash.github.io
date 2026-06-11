0ee9cc375171e0328183b1e2b0a66c9bd64810dd
knappyflash <knappyflash2@gmail.com>
2026-06-10 20:33:16 -0700

    rs nor latch

---
+++ logic_gates copy/QuickTestSketch.js
@@ -0,0 +1,54 @@
+import Bulb from "./Bulb.js";
+import Lever from "./Lever.js";
+import NotGate from "./NotGate.js";
+import OrGate from "./OrGate.js";
+
+export const QuickTestSketch = (p) => {
+  let bg = [150, 150, 200, 255];
+
+  let lever1 = new Lever(p);
+  let lever2 = new Lever(p);
+  let notGate1 = new NotGate(p);
+  let notGate2 = new NotGate(p);
+  let notGate3 = new NotGate(p);
+  let orGate1 = new OrGate(p);
+  let bulb1 = new Bulb(p);
+
+
+  p.setup = () => {
+    const parent = document.getElementById("quickTestDiv");
+    let w = parent.clientWidth;
+    let h = parent.clientHeight;
+    let canvas = p.createCanvas(w-20, h-20);
+    canvas.parent("quickTestDiv");
+    p.background(bg[0], bg[1], bg[2], bg[3]);
+    UpdateLogicGates();
+  }
+
+  p.draw = () => {
+  }
+
+  p.mousePressed = () => {
+  }
+
+  p.mouseReleased = () => {
+    UpdateLogicGates();
+  }
+
+  function UpdateLogicGates(){
+    p.background(bg[0], bg[1], bg[2], bg[3]);
+    lever1.Update(30,70);
+    lever2.Update(30,200);
+    notGate1.Update(100,120,lever1);
+    notGate2.Update(100,250,lever2);
+    orGate1.Update(200,125,notGate1,notGate2);
+    notGate3.Update(320,183,orGate1);
+    bulb1.Update(450,117,notGate3);
+
+    p.fill(255, 255, 255);
+    p.strokeWeight(3);
+    p.stroke(0, 0, 0);
+    p.textSize(50);
+    p.text('And Gate', 125, 50);
+  }
+}
