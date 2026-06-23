36afa90668af142109ed235ef6c1106a0962e41d
knappyflash <knappyflash2@gmail.com>
2026-06-23 05:48:22 -0700

    test

---
+++ programming_concepts/programming_concepts.js
@@ -0,0 +1,10 @@
+const pageTitle = "programming_concepts";
+window.addEventListener("load", function () {
+    console.log(pageTitle + " Page is loaded");
+    changeTitle();
+});
+function changeTitle() {
+  const headerFrame = parent.frames["header"];
+  const title = headerFrame.document.getElementById("topTitle");
+  title.textContent = pageTitle;
+}
